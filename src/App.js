254c8cd3d0f99b36625ccb0ecac3fbd6027ad285
sotsuna <mkist202@gmail.com>
2023-11-10 15:52:13 -0300

    marcos é burro pt2

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const App = () => (
     <Navbar/>
     <Content text={text[1].text} title={text[1].title} inverted/>
     <Content text={text[2].text} title={text[2].title}/>
-    <Content text={text[3].text} title={text[3].title}/>
+    <Content text={text[3].text} title={text[3].title} inverted/>
     <Content text={text[4].text} title={text[4].title}/>
-    <Content text={text[5].text} title={text[5].title}/>
+    <Content text={text[5].text} title={text[5].title} inverted/>
   </div>
 );
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,18 @@
 const Content = ({text, title, inverted = false}) => {
 
     const invertede = inverted ? "order-last" : " ";
-    const classes = `h-screen w-full grid gap-x-8 gap-y-4 grid-cols-2 items-center px-8 ${invertede}`;
+    const classes = `text-center ${invertede}`;
 
     return (
         <div>
-            <div className={classes}>
-                <div className="text-center">
-                    <h1>
+            <div className="h-screen w-full grid gap-x-8 gap-y-4 grid-cols-2 items-center px-8">
+                <div className={classes}>
+                    <h1 className=" text-black text-4xl font-extrabold md:text-5xl">
                         {title}
                     </h1>
                 </div>
                 <div>
-                    <p className="text-center">
+                    <p className={classes}>
                         {text}
                     </p>
                 </div>
