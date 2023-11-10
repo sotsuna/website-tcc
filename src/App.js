0529b21da54d0508d11edb9c7509a9319add77d0
sotsuna <mkist202@gmail.com>
2023-11-10 19:08:47 -0300

    não sei mais o que eu estou fazendo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Content from './components/Content';
 import Navbar from './components/Navbar';
 import text from './components/text.json';
+import Sidebar from './components/Sidebar';
 
 const App = () => (
   <div>
     <Navbar/>
+    <Sidebar/>
     <Content text={text[1].text} title={text[1].title} inverted/>
     <Content text={text[2].text} title={text[2].title}/>
-    <Content text={text[3].text} title={text[3].title}/>
+    <Content text={text[3].text} title={text[3].title} inverted/>
     <Content text={text[4].text} title={text[4].title}/>
-    <Content text={text[5].text} title={text[5].title}/>
+    <Content text={text[5].text} title={text[5].title} inverted/>
   </div>
 );
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,18 @@
 const Content = ({text, title, inverted = false}) => {
 
     const invertede = inverted ? "order-last" : " ";
-    const classes = `h-screen w-full grid gap-x-8 gap-y-4 grid-cols-2 items-center px-8 ${invertede}`;
+    const classes = `text-center ${invertede}`;
 
     return (
         <div>
-            <div className={classes}>
-                <div className="text-center">
-                    <h1>
+            <div className="h-screen w-full grid gap-x-8 gap-y-4 grid-cols-2 items-center px-8">
+                <div className={classes}>
+                    <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
                         {title}
                     </h1>
                 </div>
                 <div>
-                    <p className="text-center">
+                    <p className={classes}>
                         {text}
                     </p>
                 </div>
