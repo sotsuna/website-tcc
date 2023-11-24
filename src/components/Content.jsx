4568d0d30b863cc7f5eadb31551e207d15cd7403
sotsuna <mkist202@gmail.com>
2023-11-24 12:13:41 -0300

    quase pronto finalmente

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,13 +3,13 @@ import '../App.css'
 const Content = ({ id, text, title, inverted = false }) => {
 
     const invertede = inverted ? "order-last" : " ";
-    const classes = `text-center ${invertede}`;
+    const classes = `text-center fade-in-left ${invertede}`;
 
     return (
 
-        <div className="h-screen w-full grid gap-x-8 gap-y-4 grid-cols-2 items-center px-8 text-dark-green-gray" id={id}>
+        <div className="h-screen w-full grid gap-x-8 gap-y-4 grid-cols-2 items-center px-8 text-dark-green-gray will-change-auto whitespace-pre-line" id={id}>
             <div className={classes}>
-                <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                <h1 className="fade-in-left text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
                     {title}
                 </h1>
             </div>
